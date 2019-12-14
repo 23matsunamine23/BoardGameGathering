d0e977abd200e3ba7438614a806823644d93030f
23matsunamine23 <noname0323namine@gmail.com>
2019-12-14 15:10:54 +0900

    tweetsテーブルの作成

--- db/migrate/20191214052534_create_tweets.rb
+++ db/migrate/20191214052534_create_tweets.rb
@@ -1,7 +1,10 @@
 class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
-
+      t.text :body
+      t.string :image_1
+      t.string :image_2
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_14_050258) do
+ActiveRecord::Schema.define(version: 2019_12_14_052534) do
+
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "body"
+    t.string "image_1"
+    t.string "image_2"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +38,5 @@ ActiveRecord::Schema.define(version: 2019_12_14_050258) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
