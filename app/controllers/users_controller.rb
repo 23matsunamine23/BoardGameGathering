b2fb84dfbe415ff9b95480e3a8cb2eb8cf7b77bc
23matsunamine23 <noname0323namine@gmail.com>
2019-12-21 13:54:59 +0900

    usersコントローラーにset_userを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user
+
   def show
-    @user = User.find(params[:id])
   end
+  
   def edit
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
   end
 end
