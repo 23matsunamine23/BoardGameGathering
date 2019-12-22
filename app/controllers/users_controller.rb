ac81f0842909a5741e79f902e09c44268ff6c5ed
23matsunamine23 <noname0323namine@gmail.com>
2019-12-22 17:07:40 +0900

    usersコントローラーにupdateアクションを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,24 @@ class UsersController < ApplicationController
 
   def show
   end
-  
+
   def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :avatar, :profile, :sex)
   end
 end
