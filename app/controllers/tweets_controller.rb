45585f8bd0ee1224c30ce9bdbc6393d00c144499
23matsunamine23 <noname0323namine@gmail.com>
2019-12-23 15:56:15 +0900

    tweetsコントローラーにshowアクションの定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,6 +11,8 @@ class TweetsController < ApplicationController
     @tweet.save
     render "tops/index"
   end
+  def show
+  end
   def mine
     @tweets = current_user.tweets.order(created_at: :desc).page(params[:page]).per(10)
   end
