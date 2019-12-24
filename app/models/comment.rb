98e3df3ad550f890ff5951c29a9ee675c8277061
23matsunamine23 <noname0323namine@gmail.com>
2019-12-24 23:43:56 +0900

    userとcommentのアソシエーションを削除

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,3 @@
 class Comment < ApplicationRecord
   belongs_to :tweet
-  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,4 @@ class User < ApplicationRecord
           :recoverable, :rememberable, :validatable
           mount_uploader :avatar, ImageUploader
           has_many :tweets
-          has_many :comments
 end
