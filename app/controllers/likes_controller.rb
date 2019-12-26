61b69ca35007aa0573be960125e37c621b53ffa7
23matsunamine23 <noname0323namine@gmail.com>
2019-12-26 23:35:29 +0900

    likesコントローラーの追加

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,12 @@
+class LikesController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    @like = current_user.likes.create(tweet_id: params[:tweet_id])
+    redirect_to tweets_path
+  end
+  def delete
+    @like = Like.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
+    @like.destroy
+    redirect_to tweets_path
+  end
+end
