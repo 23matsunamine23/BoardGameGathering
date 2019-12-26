f1538a21e02f0243f22cbd000a49874624cc2496
23matsunamine23 <noname0323namine@gmail.com>
2019-12-26 23:36:20 +0900

    いいね機能のアソシエーションを追加

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,6 @@ class Tweet < ApplicationRecord
   has_many :comments, dependent: :destroy
   mount_uploader :image1, ImageUploader
   mount_uploader :image2, ImageUploader
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise  :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
-          mount_uploader :avatar, ImageUploader
-          has_many :tweets
-          has_many :comments
+  mount_uploader :avatar, ImageUploader
+  has_many :tweets
+  has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :liked_tweets, through: :likes, source: :post
+  def already_liked?(tweet)
+    self.likes.exists?(tweet_id: tweet.id)
+  end
 end
