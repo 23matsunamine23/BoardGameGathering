755504e02e32195c881509ce71e584c91c019f13
23matsunamine23 <noname0323namine@gmail.com>
2019-12-30 16:13:32 +0900

    フォロー機能のルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users do
     resource :relationships, only: [:create, :destroy]
     member do
-      get :follows
+      get :followings
       get :followers
     end
   end
