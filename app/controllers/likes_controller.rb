38c7038c43727c11b033dae9d7eb5230d0c2a81b
23matsunamine23 <noname0323namine@gmail.com>
2020-01-11 17:07:53 +0900

    フォロー機能の非同期通信を追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,12 +3,12 @@ class LikesController < ApplicationController
   def create
     @tweet = Tweet.find(params[:tweet_id])
     @like = current_user.likes.create(tweet_id: params[:tweet_id])
-    render 'create.js.erb'
+    render 'likes/create.js.erb'
   end
   def destroy
     @tweet = Tweet.find(params[:tweet_id])
     @like = Like.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
     @like.destroy
-    render 'destroy.js.erb'
+    render 'likes/destroy.js.erb'
   end
 end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,15 @@
 class RelationshipsController < ApplicationController
   def create
+    @user = User.find(params[:user_id])
     follow = current_user.active_relationships.new(follower_id: params[:user_id])
     follow.save
-    redirect_to user_path(params[:user_id])
+    render 'relationships/create.js.erb'
   end
 
   def destroy
+    @user = User.find(params[:user_id])
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
     follow.destroy
-    redirect_to user_path(params[:user_id])
+    render 'relationships/destroy.js.erb'
   end
 end
