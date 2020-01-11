457a0d1249e58453e3360c9a5bcdc18bd8a60789
23matsunamine23 <noname0323namine@gmail.com>
2020-01-11 14:33:26 +0900

    いいね機能の非同期機能を追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,14 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
   def create
+    @tweet = Tweet.find(params[:tweet_id])
     @like = current_user.likes.create(tweet_id: params[:tweet_id])
-    redirect_to tweets_path
+    render 'create.js.erb'
   end
   def destroy
+    @tweet = Tweet.find(params[:tweet_id])
     @like = Like.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
     @like.destroy
-    redirect_to tweets_path
+    render 'destroy.js.erb'
   end
 end
